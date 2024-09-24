58b352b0aa7d90b36975e5e743b3b0b8fe9cb382
Manida Vilay <vilaymanida@gmail.com>
2024-09-24 11:45:53 +0200

    create task slice with addtask action

---
+++ lib/tasksSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+interface Task {
+    id: string,
+    title: string,
+    frequency: "daily" | "weekly" | "monthly",
+    completed: string[],
+    createdAt: string
+}
+
+interface TasksState {
+    tasks: Task[]
+}
+
+const initialState: TasksState = {
+    tasks: []
+}
+
+const tasksSlice = createSlice({
+    name: "tasks",
+    initialState,
+    reducers: {
+        addTask: (state, action: PayloadAction<{ title: string, frequency: "daily" | "weekly" | "monthly" }>) => {
+            const newTask = {
+                id: Date.now().toString(),
+                title: action.payload.title,
+                frequency: action.payload.frequency,
+                completed: [],
+                createdAt: new Date().toISOString()
+            }
+            state.tasks.push(newTask)
+        }
+    }
+})
+
+export const { addTask } = tasksSlice.actions
+export default tasksSlice.reducer
