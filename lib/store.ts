61b015922d5ccd42311eef418a54e25dd04f21f7
Manida Vilay <vilaymanida@gmail.com>
2024-09-24 12:21:31 +0200

    create store

---
+++ lib/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tasksReducer from "./tasksSlice"
+
+const store = configureStore({
+    reducer: tasksReducer
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export default store
