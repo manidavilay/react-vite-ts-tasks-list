3289f5772bc771ed941e2939f3a9102c235fe55c
Manida Vilay <vilaymanida@gmail.com>
2024-09-24 19:17:04 +0200

    add editing task action

--- lib/tasksSlice.ts
+++ lib/tasksSlice.ts
@@ -1,54 +1,81 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface Task {
-    id: string,
-    title: string,
-    frequency: "daily" | "weekly" | "monthly",
-    completed: string[],
-    createdAt: string
+  id: string;
+  title: string;
+  frequency: "daily" | "weekly" | "monthly";
+  completed: string[];
+  createdAt: string;
 }
 
 interface TaskState {
-    tasks: Task[]
+  tasks: Task[];
 }
 
 const initialState: TaskState = {
-    tasks: []
-}
+  tasks: [],
+};
 
 const tasksSlice = createSlice({
-    name: "tasks",
-    initialState,
-    reducers: {
-        addTask: (state, action: PayloadAction<{ title: string, frequency: "daily" | "weekly" | "monthly" }>) => {
-            const newTask = {
-                id: Date.now().toString(),
-                title: action.payload.title,
-                frequency: action.payload.frequency,
-                completed: [],
-                createdAt: new Date().toISOString()
-            }
-            state.tasks.push(newTask)
-        },
-        toggleStatus: (state, action: PayloadAction<{ id: string, date: string }>) => {
-            const foundTask = state.tasks.find(task => task.id === action.payload.id)
-            if (foundTask) {
-                const foundTaskIndex = foundTask.completed.indexOf(action.payload.date)
-                if (foundTaskIndex > -1) {
-                    foundTask.completed.splice(foundTaskIndex, 1)
-                } else {
-                    foundTask.completed.push(action.payload.date)
-                }
-            }
-        },
-        removeTask: (state, action: PayloadAction<{ id: string }>) => {
-            const foundTaskIndex = state.tasks.findIndex(task => task.id === action.payload.id)
-            if (foundTaskIndex > -1) {
-                state.tasks.splice(foundTaskIndex, 1)
-           }
-        }  
-    }
-})
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTask: (
+      state,
+      action: PayloadAction<{
+        title: string;
+        frequency: "daily" | "weekly" | "monthly";
+      }>
+    ) => {
+      const newTask = {
+        id: Date.now().toString(),
+        title: action.payload.title,
+        frequency: action.payload.frequency,
+        completed: [],
+        createdAt: new Date().toISOString(),
+      };
+      state.tasks.push(newTask);
+    },
+    editTask: (
+      state,
+      action: PayloadAction<{
+        id: string;
+        title: string;
+        frequency: "daily" | "weekly" | "monthly";
+      }>
+    ) => {
+        const taskToEdit = state.tasks.find((task) => task.id === action.payload.id);
+        if (taskToEdit) {
+          taskToEdit.title = action.payload.title;
+          taskToEdit.frequency = action.payload.frequency;
+        }
+    },
+    toggleStatus: (
+      state,
+      action: PayloadAction<{ id: string; date: string }>
+    ) => {
+      const foundTask = state.tasks.find(
+        (task) => task.id === action.payload.id
+      );
+      if (foundTask) {
+        const foundTaskIndex = foundTask.completed.indexOf(action.payload.date);
+        if (foundTaskIndex > -1) {
+          foundTask.completed.splice(foundTaskIndex, 1);
+        } else {
+          foundTask.completed.push(action.payload.date);
+        }
+      }
+    },
+    removeTask: (state, action: PayloadAction<{ id: string }>) => {
+      const foundTaskIndex = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      if (foundTaskIndex > -1) {
+        state.tasks.splice(foundTaskIndex, 1);
+      }
+    },
+  },
+});
 
-export const { addTask, toggleStatus, removeTask } = tasksSlice.actions
-export default tasksSlice.reducer
+export const { addTask, editTask, toggleStatus, removeTask } = tasksSlice.actions;
+export default tasksSlice.reducer;
--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,20 +1,59 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../lib/store";
-import { toggleStatus, removeTask, Task } from "../../lib/tasksSlice";
+import { toggleStatus, removeTask, Task, editTask } from "../../lib/tasksSlice";
 import {
   Button,
+  FormControl,
   Grid2,
   LinearProgress,
+  MenuItem,
   Paper,
+  Select,
+  TextField,
   Typography,
 } from "@mui/material";
 import CheckCircleOutlineOutlinedIcon from "@mui/icons-material/CheckCircleOutlineOutlined";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DoneOutlinedIcon from "@mui/icons-material/DoneOutlined";
 
 const TasksList = () => {
+  const [isCurrentlyEditingId, setIsCurrentlyEditingId] = useState<
+    string | null
+  >(null);
+  const [editedTitle, setEditedTitle] = useState<string>("");
+  const [editedFrequency, setEditedFrequency] = useState<
+    "daily" | "weekly" | "monthly"
+  >("daily");
   const { tasks } = useSelector((state: RootState) => state.tasks);
   const dispatch = useDispatch<AppDispatch>();
   const today = new Date().toISOString().split("T")[0];
+
+  const handleEditingID = (e: React.FormEvent, id: string) => {
+    e.preventDefault();
+    const taskToEdit = tasks.find((task) => task.id === id);
+    if (taskToEdit) {
+      setEditedTitle(taskToEdit.title);
+      setEditedFrequency(taskToEdit.frequency);
+      setIsCurrentlyEditingId(id);
+    }
+  };
+
+  const handleTaskEditSubmit = (e: React.FormEvent, id: string) => {
+    e.preventDefault();
+    const task = tasks.find((task) => task.id === id);
+    if (task) {
+      dispatch(
+        editTask({
+          id: id,
+          title: editedTitle,
+          frequency: editedFrequency,
+        })
+      );
+    }
+    setIsCurrentlyEditingId(null);
+  };
 
   const getStreak = (task: Task) => {
     const currentDate = new Date();
@@ -31,76 +70,128 @@ const TasksList = () => {
     return streak;
   };
 
-  return (
-    <section>
-      {tasks.map((task) => {
-        return (
-          <Paper key={task.id} sx={{ p: 3, mt: 4 }}>
+  return tasks.map((task) => {
+    return (
+      <Paper key={task.id} sx={{ p: 3, mt: 4 }}>
+        <form
+          onSubmit={(e) =>
+            isCurrentlyEditingId !== task.id
+              ? handleEditingID(e, task.id)
+              : handleTaskEditSubmit(e, task.id)
+          }
+        >
+          <Grid2
+            container
+            size={12}
+            sx={{
+              display: "flex",
+              flexDirection: { md: "row", xs: "column" },
+            }}
+          >
             <Grid2
-              container
-              size={12}
+              size={{ md: 6, xs: 12 }}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                mb: { xs: 2 },
+              }}
+            >
+              {isCurrentlyEditingId !== task.id ? (
+                <>
+                  <Typography variant="h6">{task.title}</Typography>
+                  <Typography variant="body2">{task.frequency}</Typography>
+                </>
+              ) : (
+                <div>
+                  <TextField
+                    placeholder="Edit Task"
+                    fullWidth
+                    onChange={(e) => setEditedTitle(e.target.value)}
+                    sx={{ mb: 2 }}
+                    value={editedTitle || task.title}
+                  />
+                  <FormControl fullWidth sx={{ mb: 2 }}>
+                    <Select
+                      value={editedFrequency || task.frequency}
+                      onChange={(e) =>
+                        setEditedFrequency(
+                          e.target.value as "daily" | "weekly" | "monthly"
+                        )
+                      }
+                    >
+                      <MenuItem value="daily">Daily</MenuItem>
+                      <MenuItem value="weekly">Weekly</MenuItem>
+                      <MenuItem value="monthly">Monthly</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+              )}
+            </Grid2>
+            <Grid2
+              size={{ md: 6, xs: 12 }}
               sx={{
                 display: "flex",
                 flexDirection: { md: "row", xs: "column" },
+                alignItems: { md: "center", xs: "flex-start" },
+                justifyContent: { md: "flex-end" },
+                mb: 2,
               }}
             >
-              <Grid2
-                sx={{ display: "flex", flexDirection: "column", mb: { xs: 2 } }}
-                size={{ md: 6, xs: 12 }}
+              <Button
+                variant="outlined"
+                color={task.completed.includes(today) ? "success" : "primary"}
+                sx={{ my: 1, mr: 1 }}
+                startIcon={
+                  task.completed.includes(today) ? (
+                    <CheckCircleOutlineOutlinedIcon />
+                  ) : (
+                    <CircleOutlinedIcon />
+                  )
+                }
+                onClick={() =>
+                  dispatch(toggleStatus({ id: task.id, date: today }))
+                }
               >
-                <Typography variant="h6">{task.title}</Typography>
-                <Typography variant="body2">{task.frequency}</Typography>
-              </Grid2>
-              <Grid2
-                size={{ md: 6, xs: 12 }}
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: { md: "flex-end" },
-                }}
+                {task.completed.includes(today) ? "Completed" : "Mark Complete"}
+              </Button>
+              <Button
+                type="submit"
+                variant="outlined"
+                color="secondary"
+                sx={{ my: 1, mr: 1 }}
+                startIcon={
+                  isCurrentlyEditingId !== task.id ? (
+                    <EditOutlinedIcon />
+                  ) : (
+                    <DoneOutlinedIcon />
+                  )
+                }
               >
-                <Button
-                  variant="outlined"
-                  color={task.completed.includes(today) ? "success" : "primary"}
-                  sx={{ mr: 1 }}
-                  startIcon={
-                    task.completed.includes(today) ? (
-                      <CheckCircleOutlineOutlinedIcon />
-                    ) : (
-                      <CircleOutlinedIcon />
-                    )
-                  }
-                  onClick={() =>
-                    dispatch(toggleStatus({ id: task.id, date: today }))
-                  }
-                >
-                  {task.completed.includes(today)
-                    ? "Completed"
-                    : "Mark Complete"}
-                </Button>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => dispatch(removeTask({ id: task.id }))}
-                >
-                  Remove Task
-                </Button>
-              </Grid2>
+               {isCurrentlyEditingId !== task.id ? "Edit Task" : "Done Editing"}
+              </Button>
+              <Button
+                sx={{ my: 1 }}
+                variant="outlined"
+                color="error"
+                onClick={() => dispatch(removeTask({ id: task.id }))}
+              >
+                Remove Task
+              </Button>
             </Grid2>
-            <Grid2 sx={{ mt: 2 }}>
-              <Typography variant="body2">
-                Current streak: {getStreak(task)} days
-              </Typography>
-              <LinearProgress
-                variant="determinate"
-                value={(getStreak(task) / 30) * 100}
-              />
-            </Grid2>
-          </Paper>
-        );
-      })}
-    </section>
-  );
+          </Grid2>
+        </form>
+        <div>
+          <Typography variant="body2">
+            Current streak: {getStreak(task)} days
+          </Typography>
+          <LinearProgress
+            variant="determinate"
+            value={(getStreak(task) / 30) * 100}
+          />
+        </div>
+      </Paper>
+    );
+  });
 };
 
 export default TasksList;
